fe569520cd11dba462e4d6ddacd1aebc5e5e77cf
Amp <alexbest@utexas.edu>
2016-09-28 19:07:14 -0500

    User Logins Working?

--- app/models.py
+++ app/models.py
@@ -6,6 +6,25 @@ class User(db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @propery
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        try:
+            return unicode(self.id) # Python2
+        except NameError:
+            return str(self.id) #Python3
+
+
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
